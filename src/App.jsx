556f24f6eb2e574a0790e66303a1bd30218c177b
Sandeep <your-email@example.com>
2025-04-15 12:20:56 +0530

    add new components and how to crate  props and and how to use props

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+// import img 
+// import reactImg from './assets/react.svg'
 
 const reactDescriptions = ["komal", "preet" , "singh" , " dhillon"]
 
@@ -12,7 +14,7 @@ function reactRandomImt(max) {
 const description = reactDescriptions[reactRandomImt(4)];
 
 
-function ImgFunction() {
+/* function ImgFunction() {
   return (
     <header>
       <a href="https://vite.dev" target="_blank">
@@ -23,6 +25,18 @@ function ImgFunction() {
         </a>
     </header>
   );
+} */
+
+// create the new component 
+
+function CoreConcept(props){
+  return(
+    <a>
+      <img src={props.img} alt="" className="logo react"/>
+    {/*   <h3>komal</h3>
+      <p>descretion</p> */}
+    </a>
+  );
 }
 
 
@@ -32,7 +46,21 @@ function App() {
   return (
     <>
       <div>
-        <ImgFunction />
+        {/* <ImgFunction /> */}
+        <CoreConcept 
+          img={viteLogo}
+       /*    title = "components"
+          description = "this is ui" */
+        />
+        <CoreConcept 
+          img={reactLogo}
+
+        />
+       {/*   <CoreConcept 
+          img={reactLogo}
+
+        /> */}
+
       </div>
        <h1>Vite + React</h1>
       <div className="card">
@@ -51,23 +79,8 @@ function App() {
     </>
   )
 }
-/*  function count( { count, setCount }) {
-  return (
-    <header>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </header>
-  );
-} */
+
  
 export default App
+
+
